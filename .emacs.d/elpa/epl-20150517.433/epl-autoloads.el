d60b4dbb0c3f0e7e39ffc3da58b59c5a3f658011
Oskar Strömberg <oskstr@kth.se>
2017-02-27 13:26:38 +0100

    Hopefully fixed things

---
+++ .emacs.d/elpa/epl-20150517.433/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl.el") (22691 58742 834349 386000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
