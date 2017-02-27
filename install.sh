d5d5d9fc7b293f4dd077cdce9fd75249918f28db
Oskar Strömberg <oskstr@kth.se>
2017-02-27 13:18:51 +0100

    Deleted some files.

--- install.sh
+++ install.sh
@@ -5,28 +5,28 @@ mkdir -p ~/.ssh
 mkdir -p ~/.newsbeuter
 
 # Link files from dotfiles
-ln -s ~/.dotfiles/authinfo.gpg ~/.authinfo.gpg
-ln -s ~/.dotfiles/bashrc ~/.bashrc
-ln -s ~/.dotfiles/bashrc.d ~/.bashrc.d
-ln -s ~/.dotfiles/bin ~/.bin
-ln -s ~/.dotfiles/emacs.d ~/.emacs.d
-ln -s ~/.dotfiles/git ~/.git
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/gitignore ~/.gitignore
-ln -s ~/.dotfiles/gitmessage ~/.gitmessage
-ln -s ~/.dotfiles/i3 ~/.i3
-ln -s ~/.dotfiles/i3status.conf ~/.i3status.conf
-ln -s ~/.dotfiles/mbsyncrc ~/.mbsyncrc
-ln -s ~/.dotfiles/msmtprc ~/.msmtprc
-ln -s ~/.dotfiles/mutt ~/.mutt
-ln -s ~/.dotfiles/newsbeuter-config ~/.newsbeuter/config
-ln -s ~/.dotfiles/rspec ~/.rspec
-ln -s ~/.dotfiles/rubocop.yml ~/.rubocop.yml
-ln -s ~/.dotfiles/sbclrc ~/.sbclrc
-ln -s ~/.dotfiles/ssh-config ~/.ssh/config
-ln -s ~/.dotfiles/urlview ~/.urlview
-ln -s ~/.dotfiles/wallpaper ~/.wallpaper
-ln -s ~/.dotfiles/xbindkeysrc ~/.xbindkeysrc
+ln -s ~/dotfiles/authinfo.gpg ~/.authinfo.gpg
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/bashrc.d ~/.bashrc.d
+ln -s ~/dotfiles/bin ~/.bin
+ln -s ~/dotfiles/.emacs.d ~/.emacs.d
+ln -s ~/dotfiles/git ~/.git
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
+ln -s ~/dotfiles/gitmessage ~/.gitmessage
+ln -s ~/dotfiles/i3 ~/.i3
+ln -s ~/dotfiles/i3status.conf ~/.i3status.conf
+ln -s ~/dotfiles/mbsyncrc ~/.mbsyncrc
+ln -s ~/dotfiles/msmtprc ~/.msmtprc
+ln -s ~/dotfiles/mutt ~/.mutt
+ln -s ~/dotfiles/newsbeuter-config ~/.newsbeuter/config
+ln -s ~/dotfiles/rspec ~/.rspec
+ln -s ~/dotfiles/rubocop.yml ~/.rubocop.yml
+ln -s ~/dotfiles/sbclrc ~/.sbclrc
+ln -s ~/dotfiles/ssh-config ~/.ssh/config
+ln -s ~/dotfiles/urlview ~/.urlview
+ln -s ~/dotfiles/wallpaper ~/.wallpaper
+ln -s ~/dotfiles/xbindkeysrc ~/.xbindkeysrc
 
 # Link .bash_profile -> .bashrc
 rm -f ~/.bash_profile
